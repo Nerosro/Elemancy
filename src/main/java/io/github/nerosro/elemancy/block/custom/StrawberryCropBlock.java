d3cedac802bc50adf3efed10267b76f2f25e2228
Nero <5601152+Nerosro@users.noreply.github.com>
2023-10-25 03:54:36 +0200

    Custom crops

---
+++ src/main/java/io/github/nerosro/elemancy/block/custom/StrawberryCropBlock.java
@@ -0,0 +1,43 @@
+package io.github.nerosro.elemancy.block.custom;
+
+import io.github.nerosro.elemancy.item.ModItems;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+/**
+ * Created by Nerosro on 25/10/2023.
+ */
+public class StrawberryCropBlock extends CropBlock {
+
+    public static final int MAX_AGE = 5;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_5;
+
+    public StrawberryCropBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE);
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    protected ItemLike getBaseSeedId() {
+        return ModItems.STRAWBERRY_SEEDS.get();
+    }
+}
